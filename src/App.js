795664c8eed186da5dbc57e7c45950df8c2428e1
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2022-02-23 12:40:54 +0530

    layout added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
 
-function App() {
+import { Navbar } from './components'
+
+const App = () => {
   return (
-    <div className="App">
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
 
+      </div>
+      <div className='footer'>
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
