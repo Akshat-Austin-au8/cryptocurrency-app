d90db07b033323e26f1d91c621cbc33b33e22fc4
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2022-02-25 14:01:46 +0530

    all done expect cryptodetail

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import 'antd/dist/antd.css'
+import App from './App';
+import store from './app/store';
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
+
